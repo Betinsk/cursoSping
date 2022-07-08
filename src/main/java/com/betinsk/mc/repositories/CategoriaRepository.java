737c48d598931168aee47682331bc8055a245950
Betinsk <gmdust@htomail.com>
2022-07-08 12:16:13 -0300

    Implementado service

---
+++ src/main/java/com/betinsk/mc/repositories/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.betinsk.mc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.betinsk.mc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+	
+}
