113f8b6188a1b1e2e2a2128d61c7baa1c5798d1a
Betinsk <gmdust@htomail.com>
2022-08-23 13:25:43 -0300

    Envio de email com servidor Google

--- src/main/java/com/betinsk/mc/config/DevConfig.java
+++ src/main/java/com/betinsk/mc/config/DevConfig.java
@@ -9,6 +9,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.betinsk.mc.services.DbService;
+import com.betinsk.mc.services.EmailService;
+import com.betinsk.mc.services.MockEmailService;
+import com.betinsk.mc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,5 +32,10 @@ public class DevConfig {
 		}
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
